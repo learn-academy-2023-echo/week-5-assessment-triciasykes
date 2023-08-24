d59bec51f6acb1d46f5a2820104b5725ec9e5566
triciasykes <tlsykes98@gmail.com>
2023-08-24 10:55:27 -0700

    in class notes for assessment review

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,45 +14,141 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
+describe("ApplySecretCode", () => {
+  it("apply Secret Code to a string", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    const secretCodeWord2 = "Gobbledygook"
+    const secretCodeWord3 = "Eccentric"
+    expect(ApplySecretCode(secretCodeWord1)).toBe("L4ck4d41s1c4l")
+    expect(ApplySecretCode(secretCodeWord2)).toBe("G0bbl3dyg00k")
+    expect(ApplySecretCode(secretCodeWord3)).toBe("3cc3ntr1c")
+  })
+})
+
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
 
+// create a fxn with input of string
+// map over characters to their secret code placements
+// initialize an empty str to store result
+// loop through each char
+// convert current char to lowercase
+// if char has secret code placement add to result
+// if not, add original char to result
+// return final result
+
+const ApplySecretCode = (inputString) => {
+  const codeMap = { a: "4", e: "3", i: "1", o: "0" }
+  let result = ""
+  for (const char of inputString) {
+    if (codeMap[char.toLowerCase()]) {
+      result += codeMap[char.toLowerCase()]
+    } else {
+      result += char
+    }
+  }
+  return result
+}
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("wordFind", () => {
+  it("finds a word in array from single letter in and returns the words with that letter", () => {
+    const fruitArray = [
+      "Mango",
+      "Cherry",
+      "Apricot",
+      "Blueberry",
+      "Peach",
+      "Kiwi",
+    ]
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
+    const filterLetterA = "a"
+    // Expected output: ["Mango", "Apricot", "Peach"]
+    const filterLetterE = "e"
+
+    expect(wordFind(fruitArray, filterLetterA)).toEqual([
+      "Mango",
+      "Apricot",
+      "Peach",
+    ])
+    expect(wordFind(fruitArray, filterLetterE)).toEqual([
+      "Cherry",
+      "Blueberry",
+      "Peach",
+    ])
+  })
+})
+
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// create HOF that takes an array and a char
+// use .filter to iterate thru array with built in if
+// char to lowercase
+// use .includes with char
+// return result
+
+const wordFind = (array, letter) => {
+  return array.filter((word) =>
+    word.toLowerCase().includes(letter.toLowerCase())
+  )
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
+describe("fullHouse", () => {
+  it("whether or not array is fullhouse", () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    // Expected output: true
+    const hand2 = [5, 5, 3, 3, 4]
+    // Expected output: false
+    const hand3 = [5, 5, 5, 5, 4]
+    // Expected output: false
+    const hand4 = [7, 2, 7, 2, 7]
+    expect(fullHouse(hand1)).toEqual("true")
+    expect(fullHouse(hand2)).toEqual("false")
+    expect(fullHouse(hand3)).toEqual("false")
+    expect(fullHouse(hand4)).toEqual("true")
+  })
+})
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
-
 // b) Create the function that makes the test pass.
+// pseudo:
+// input: array of numbers (see above)
+// output: array with 2 and 3 of a kind to equal full house (hand 1 and hand 4)
+
+// process:
+// sort the numbers
+// index 0 and 1 and 2,3,4 OR 0,1,2 and 3,4  returns true
+// if not returns false
+// conditionals
+// determine value in the array
+
+const fullHouse = (array) => {
+  let sortedArray = array.sort()
+  if (
+    (sortedArray[0] === sortedArray[1] &&
+      sortedArray[2] === sortedArray[3] &&
+      sortedArray[3] === sortedArray[4]) ||
+    (sortedArray[0] === sortedArray[1] &&
+      sortedArray[1] === sortedArray[2] &&
+      sortedArray[3] === sortedArray[4])
+  ) {
+    return "true"
+  } else {
+    return "false"
+  }
+}
 
 // Pseudo code:
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -11,22 +11,97 @@ filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
 # Pseudo code:
+# pass 2 args in method, array and letter
+# use select method, selects word from array and words including that letter
+# use .include? method
+# { |word| words.include?(letter)}
+# p returnMe(array, letter)
+
+def returnMe(array, letter)
+  array.select { |word| word.include?(letter)}
+end
+#  p returnMe(beverages_array,filter_letter_o) 
+#  p returnMe(beverages_array,filter_letter_t) 
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+# us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+# def fxn alphabetize takes in a hash
+#  create empty results array
+#  access hash values and shovel all values into result
+#  return flattend result array and sort it
 
+ def alphabetize hash
+  result = []
+  array_of_values = hash.values
+  result << array_of_values
+  result.flatten.sort 
+ end
+
+#  or hash.values.flatten.sort
+#  p alphabetize us_states
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 # Pseudo code:
+# create class called Bike
+# create initialize method with model, wheels, current speed
+# initialize val of wheels to 2
+# initialize default val of 0 to current speed
+# use attr_accessor and pass in symbols before initialize
+# create bike_info method
+#  return sentence with all data from bike obj
 
+class Bike
+  attr_accessor :model, :wheels, :current_speed
+  def initialize(model)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+  end
+
+  def bike_info
+    "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+  end
+
+  def pedal_faster num
+    @current_speed += num
+  end
+
+  def brake num
+    @current_speed -= num
+    if @current_speed < 0
+      @current_speed = 0
+    end
+    @current_speed
+  end
+end
+bike = Bike.new("Trek")
+# p bike.bike_info
+
+p bike.pedal_faster(10)
+p bike.pedal_faster(18)
+p bike.brake(5)
+p bike.brake(25)
+# "The Trek bike has 2 wheels and is going 0 mph."
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+
+# create a pedal_faster method inside of Bike class
+# pass in num as parameter
+# increment speed by given num
+# create brake method inside bike class
+# pass in num as param
+# decrement speed by given num
+#  if speed < 0 assign speed = 0
+# return current_speed
+
+
+
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
